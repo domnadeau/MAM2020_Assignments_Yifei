00713f5e15e01b864e1a9b13ffbec5779e79fe04
domnadeau <54983992+domnadeau@users.noreply.github.com>
2019-09-07 03:59:37 +0100

    Update group_10_python.py

--- group_10_python.py
+++ group_10_python.py
@@ -32,12 +32,12 @@ def question_3():
 	print('type: ' + str(type(x)))
 	print('value: ' + str(x))
 	print('The answer to b is')
-	print('The third one actually create a tuple because a tuple has to contain a sequence of values even when the length of the sequence is one')
+	print('The third one actually create a tuple because a tuple has to contain a sequence of values even when the length of the sequence is one') # this is false, an empty tuple without any sequence of values can be created by doing = ()
 	print('The answer to c is')
-	print('No it does not. The data type of variable L is ' + str(type(L)))
+	print('No it does not. The data type of variable L is ' + str(type(L))) # it is possible, see the Jupyter notebook file
 	print('The answer to d is')
 	print('The first line of code means a list variable of length 1 called L is created')
-	print('The second line of code performs a slicing operation on L in the first position')
+	print('The second line of code performs a slicing operation on L in the first position') # it is not a slicing operation if it is for only one item, it is called an indexing operation
 
 def question_4():
 	sum = 0
@@ -50,7 +50,7 @@ def question_4():
 	print('For loop is used when a index variable is required in the computation and a specified number of iterations need to be achieved')
 	print('While loop is used when a certain condition needs to be satisfied before the iterative process stops')
 	print('The answer to a ii is')
-	print('Yes it is correct')
+	print('Yes it is correct') # not exactly, there is a nuance to be made. See the Jupyter notebook file
 	print('The answer to b is')
 	print('The sum is ' + str(sum))
 	print('The answer to d is')
